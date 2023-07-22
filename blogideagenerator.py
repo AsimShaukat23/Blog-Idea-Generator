ec164807ec8bc88d527078bf44fce549a568b142
AsimShaukat23 <140038587+AsimShaukat23@users.noreply.github.com>
2023-07-22 10:57:32 -0500

    blogideagenerator.py

--- blogideagenerator.py
+++ blogideagenerator.py
@@ -1,6 +1,6 @@
 import os
-import openai
-import random
+import requests
+import streamlit as st
 
 # Define the intents
 intents = ["Informational", "Commercial", "Navigational", "Transactional"]
@@ -15,33 +15,37 @@ themes = {
 
 # Function to generate blog ideas using the OpenAI API
 def generate_blog_ideas(prompt):
-    openai.api_key = os.getenv('OPENAI_KEY')  # Get the API key from the environment variable
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=60
-    )
-    return response.choices[0].text.strip()
+    openai_api_key = os.getenv('OPENAI_KEY')
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}",
+    }
+    data = {
+        "engine": "text-davinci-002",
+        "prompt": prompt,
+        "max_tokens": 60
+    }
+    response = requests.post("https://api.openai.com/v1/engines/text-davinci-002/completions", json=data, headers=headers)
+    return response.json()["choices"][0]["text"].strip()
 
-# Ask the user to select an intent
-print("Please select an intent:")
-for i, intent in enumerate(intents, 1):
-    print(f"{i}. {intent}")
-intent_index = int(input("Enter the number of your choice: ")) - 1
-intent = intents[intent_index]
+def main():
+    st.title("Streamlit Blog Idea Generator")
 
-# Ask the user to enter a keyword
-keyword = input("Please enter a keyword: ")
+    # Ask the user to select an intent
+    intent = st.selectbox("Please select an intent:", intents)
 
-# Show the themes for the selected intent
-print("Please select a theme:")
-for i, theme in enumerate(themes[intent], 1):
-    print(f"{i}. {theme}")
-theme_index = int(input("Enter the number of your choice: ")) - 1
-theme = themes[intent][theme_index]
+    # Ask the user to enter a keyword
+    keyword = st.text_input("Please enter a keyword:")
 
-# Generate blog ideas
-prompt = f"Create a list of unique titles based on the following keyword: {keyword} {theme}. The title must have the exact keyword, must be unique and must capture the audience's attention. The title must be SEO optimized in the best ways possible. The title must be between 50-60 characters. It should not exceed the character length described."
-blog_ideas = generate_blog_ideas(prompt)
-print("Here are some blog ideas:")
-print(blog_ideas)
+    # Show the themes for the selected intent
+    theme = st.selectbox("Please select a theme:", themes[intent])
+
+    # Generate blog ideas
+    if st.button("Generate"):
+        prompt = f"Create a list of unique titles based on the following keyword: {keyword} {theme}. The title must have the exact keyword, must be unique and must capture the audience's attention. The title must be SEO optimized in the best ways possible. The title must be between 50-60 characters. It should not exceed the character length described."
+        blog_ideas = generate_blog_ideas(prompt)
+        st.subheader("Here are some blog ideas:")
+        st.write(blog_ideas)
+
+if __name__ == "__main__":
+    main()
